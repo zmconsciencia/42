9fbc751305611e6c15869b2d2f15118aeb69287e
Jose Maria Abecassis Consciencia <jabecass@c1r15s15.42lisboa.com>
2022-11-22 12:34:55 +0000

    attempt at clearing stack

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,29 @@
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:54:28 by jabecass          #+#    #+#             */
-/*   Updated: 2022/11/21 16:18:22 by jabecass         ###   ########.fr       */
+/*   Updated: 2022/11/22 11:47:33 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	clearstack(char *stack)
+{
+	int	i;
+
+	i = 0;
+	while (stack[i])
+	{
+		if (stack[i] == '\n')
+		{
+			stack[i] = 0;
+			break ;
+		}
+		i++;
+		stack[i] = 0;
+	}
+	return (i);
+}
 
 char	*get_next_line(int fd)
 {
@@ -18,16 +36,17 @@ char	*get_next_line(int fd)
 	static char	stack[BUFFER_SIZE + 1];
 
 	c = NULL;
-	while (read(fd, stack, BUFFER_SIZE))
+	while (read(fd, stack, BUFFER_SIZE) > 0)
 	{
 		c = ft_strjoin(c, stack);
+		if (clearstack(stack) != -1)
+			break ;
 	}
 	return (c);
 }
 
-//void	clearStack()
-
-/* int main(){
+/* 
+ int main(){
 	
 	int fd = open ("test", O_RDONLY);
 	char *str = get_next_line(fd);
@@ -37,4 +56,4 @@ char	*get_next_line(int fd)
 	close(fd);
 
 	return (0);
-} */
+}  */
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:49:41 by jabecass          #+#    #+#             */
-/*   Updated: 2022/11/21 16:02:45 by jabecass         ###   ########.fr       */
+/*   Updated: 2022/11/22 11:47:55 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
+int		clearstack(char *stack);
 
 #endif
