a1acdc79832c71a0bfc8454cb52427405753f646
zmconsciencia <jconsciencia1999@gmail.com>
2022-11-26 19:46:23 +0000

    final without bonus

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,37 +6,27 @@
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:54:28 by jabecass          #+#    #+#             */
-/*   Updated: 2022/11/22 11:47:33 by jabecass         ###   ########.fr       */
+/*   Updated: 2022/11/26 19:41:10 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	clearstack(char *stack)
-{
-	int	i;
-
-	i = 0;
-	while (stack[i])
-	{
-		if (stack[i] == '\n')
-		{
-			stack[i] = 0;
-			break ;
-		}
-		i++;
-		stack[i] = 0;
-	}
-	return (i);
-}
-
 char	*get_next_line(int fd)
 {
 	char		*c;
 	static char	stack[BUFFER_SIZE + 1];
+	size_t		i;
 
+	i = -1;
+	if (BUFFER_SIZE < 1 || read (fd, 0, 0) < 0)
+	{
+		while (stack[++i])
+			stack[i] = 0;
+		return (NULL);
+	}
 	c = NULL;
-	while (read(fd, stack, BUFFER_SIZE) > 0)
+	while (*stack || read(fd, stack, BUFFER_SIZE) > 0)
 	{
 		c = ft_strjoin(c, stack);
 		if (clearstack(stack) != -1)
@@ -44,16 +34,3 @@ char	*get_next_line(int fd)
 	}
 	return (c);
 }
-
-/* 
- int main(){
-	
-	int fd = open ("test", O_RDONLY);
-	char *str = get_next_line(fd);
-	
-	printf("%s", str);
-	free(str);
-	close(fd);
-
-	return (0);
-}  */
