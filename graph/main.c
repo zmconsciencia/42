3f3e7f7ee48f7035a24c44b38a1fb7fbfe778cca
Jose Maria Abecassis Consciencia <jabecass@c2r2s4.42lisboa.com>
2023-02-24 19:28:51 +0000

    chess board

--- graph/main.c
+++ graph/main.c
@@ -6,7 +6,7 @@
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:16:59 by jabecass          #+#    #+#             */
-/*   Updated: 2023/02/23 10:26:15 by jabecass         ###   ########.fr       */
+/*   Updated: 2023/02/24 19:28:18 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,72 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	put_block(t_data *data, coords starting_point, coords size, int color)
+void	put_block(t_data *data, coords starting_point, coords endpoint, int color)
 {
 	int a;
 	int b;
+	int	j;
+	int	k;
 
-	a = starting_pixel;
-	b = starting_pixel;
-	while (b < size_y)
+	a = starting_point.x;
+	b = starting_point.y;
+	j = endpoint.x;
+	k = endpoint.y;
+	while (b < k)
 	{
-		while (a++ < size_x)
+		while (a++ < j)
 			my_mlx_pixel_put(data, a, b, color);
-		a = starting_pixel;
+		a = starting_point.x;
 		b++;
+	}
+}
+
+void	put_line(t_data *data, int length, int color1, int color2, coords starting_point, coords endpoint, int controller)
+{
+	int	i;
+
+	i = 1;
+	while (i++ <= length)
+	{
+		if (i % 2 != 0)
+			put_block(data, starting_point, endpoint, color1);
+		else
+			put_block(data, starting_point, endpoint, color2);
+		starting_point.x += controller;
+		endpoint.x += controller;
 	}
 }
 
 int	main(void)
 {
-	t_data	img;
-	void	*mlx;
-	void	*mlx_win;
-	int		starting_point;
-	int		size;
+	t_data		img;
+	void		*mlx;
+	void		*mlx_win;
+	coords		starting_point;
+	coords		endpoint;
+	int			controller;
+	int			j;
 
-	starting_point = 5;
-	size = 25;
+	j = 1;
+	starting_point.x = 5;
+	starting_point.y = 5;
+	controller = 50;
+	endpoint.x = starting_point.x + controller;
+	endpoint.y = starting_point.y + controller;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Graph");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,\
 		&img.endian);
-	put_block(&img, starting_point, size, size, RED);
-    starting_point += size + 5;
-	size += starting_point;
-    put_block(&img, starting_point, size, size, WHITE);
+	while (j++ <= 8)
+	{
+		if (j % 2 != 0)
+			put_line(&img, 8, RED, LIGHT_BLUE, starting_point, endpoint, controller);
+		else
+			put_line(&img, 8, LIGHT_BLUE, RED, starting_point, endpoint, controller);
+		starting_point.y += controller;
+		endpoint.y += controller;
+	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
