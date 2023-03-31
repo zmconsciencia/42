dc79488b445d6f6424a3ed55cfe433f2deeb39f6
Jose Maria Abecassis Consciencia <jabecass@c1r7s2.42lisboa.com>
2023-03-31 15:32:24 +0100

    o diogo completou o minishell

--- pipex/main.c
+++ pipex/main.c
@@ -6,11 +6,26 @@
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:14:05 by jabecass          #+#    #+#             */
-/*   Updated: 2023/03/27 15:56:27 by jabecass         ###   ########.fr       */
+/*   Updated: 2023/03/31 14:33:04 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "header.h"
 
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = -1;
+	if (ac == 5)
+	{	
+		while (++i < ac)
+		{
+			if (i == 1 || i == 4)
+				printf("Files: %s \n", av[i]);
+			else
+				printf("Commands: %s \n", av[i]);
+		}
+	}
+	printf("Invalid number of arguments. \n");
+}
--- pipex/sources/parse.c
+++ pipex/sources/parse.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header.h                                           :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jabecass <jabecass@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/27 16:13:54 by jabecass          #+#    #+#             */
-/*   Updated: 2023/03/31 14:31:26 by jabecass         ###   ########.fr       */
+/*   Created: 2023/03/27 16:23:36 by jabecass          #+#    #+#             */
+/*   Updated: 2023/03/27 16:26:19 by jabecass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADER_H
-# define HEADER_H
+#include "header.h"
 
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <stdio.h>
-
-#endif
+void	parse(void)
+{
+	return ;
+}
